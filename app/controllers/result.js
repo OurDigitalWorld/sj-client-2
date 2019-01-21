07819087eb18fa00515b65c43ab22bb1d6ab1035
revanar <m.matt.barry@gmail.com>
2019-01-21 14:21:00 -0500

    combines the model for the english and french records into an allIssues computed value, since the all values except the URL of english and french versions of an issue should be identical.

--- app/controllers/result.js
+++ app/controllers/result.js
@@ -8,10 +8,29 @@ export default Controller.extend({
   isSerial: computed('model.category', function(){
     return this.model.category.includes('Serial');
   }),
-  sortedIssuesEng: computed.sort('issues.eng', function(a,b){
-    return compare(parseInt(a.serial_order), parseInt(b.serial_order));
+  //returns a combined model containing the attachments from the english issues, followed by the french issues.
+  issuesAll: computed('issues.{eng,fre}', function(){
+    if (this.issues.eng && this.issues.fre) {
+      let combinedIssues = [];
+      //combines issues using attachment title as the primary key
+      const engTitles = this.issues.eng.map(a => a.attachments[0].title);
+      const freTitles = this.issues.fre.map(a => a.attachments[0].title);
+      const allTitles = [...new Set([...engTitles, ...freTitles])];
+      for (let title of allTitles){
+        let engIssue = this.issues.eng ? this.issues.eng.find(a => {return a.attachments[0].title === title;}) : null;
+        const freIssue = this.issues.fre ? this.issues.fre.find(a => {return a.attachments[0].title === title;}) : null;
+        //where there's a match, adds the attachment from the french issue to the english model record.
+        engIssue.attachments.push(freIssue.attachments[0]);
+        combinedIssues.push(engIssue);
+      }
+      return combinedIssues;
+    } else if (this.issues.eng){
+      return this.issues.eng;
+    } else if (this.issues.fre){
+      return this.issues.fre;
+    }
   }),
-  sortedIssuesFre: computed.sort('issues.fre', function(a,b){
+  sortedIssuesAll: computed.sort('issuesAll', function(a,b){
     return compare(parseInt(a.serial_order), parseInt(b.serial_order));
   }),
   actions: {
